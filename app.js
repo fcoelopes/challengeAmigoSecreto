3b754c825b7c09970e73d2de71c6ec1336ec65c9
Francisco Edson Lopes da Silva <fco.edson.lopes@gmail.com>
2025-03-08 20:23:12 -0300

    Feature: Implementa função para escolher amigo

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let listaDeNomes = [];
+let listaDeIndicesSorteados = [];
+
 
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
@@ -11,4 +13,22 @@ function adicionarAmigo() {
     console.log(listaDeNomes);
     nome = document.querySelector('input');
     nome.value = '';
+}
+
+function sortearAmigo(){
+    document.getElementById('adicionar').setAttribute('disabled', true);
+    let indice = parseInt(Math.random() * listaDeNomes.length);
+    let quantidadeDeAmigosSorteados = listaDeIndicesSorteados.length;
+    if (quantidadeDeAmigosSorteados == listaDeNomes.length) {
+        document.getElementById('sortear').setAttribute('disabled', true);
+        console.log('Sorteio finalizado');
+        return;
+    }
+    if (listaDeIndicesSorteados.includes(indice)) {
+        return sortearAmigo();
+    }else{
+        listaDeIndicesSorteados.push(indice);
+    }
+    console.log(indice);
+    console.log(listaDeNomes[indice]);
 }
