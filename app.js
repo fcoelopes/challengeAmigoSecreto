7821b2c4465c76858c8fbbea9249e957871ec264
Francisco Edson Lopes da Silva <fco.edson.lopes@gmail.com>
2025-03-08 19:25:08 -0300

    Feature: Implementa função para adicionar amigos na lista

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let listaDeNomes = [];
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    if (nome && nome.trim() !== '') {
+        listaDeNomes.push(nome);
+    } else {
+        alert('Nome inválido. Por favor, forneça uma string.');
+    }
+    console.log(listaDeNomes);
+    nome = document.querySelector('input');
+    nome.value = '';
+}
